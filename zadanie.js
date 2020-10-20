ba588197e0bfdc9c190528bb0f7d53dc42367730
Michal <Michal.olearczyk@o2.pl>
2020-10-20 10:30:46 +0200

    repo update

--- zadanie.js
+++ zadanie.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/cars/', (req, res) => {
+  res.render(__dirname + '/cars.twig')
 })
 
 app.get('/cars', (req, res) => {
-  res.sendFile(__dirname + '/cars.html')
+  res.render(__dirname + '/cars.twig')
+})
+
+app.get('/cars/:brand', (req, res) => {
+  res.render(__dirname + '/model.twig')
+})
+
+app.get('/cars/:brand/:car', (req, res) => {
+  const brand=req.params.brand
+  const car=req.params.car
+  res.send(`<h3>Wybrałeś samochód ${car} marki ${brand} </h3>`);
 })
 
 app.listen(port, () => {
